c13792cc2d147f2e394a262245cf57df59996465
Sebastian-ABJ <88410272+Sebastian-ABJ@users.noreply.github.com>
2021-11-18 17:23:26 -0800

    Fixed Express import variable

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,20 @@
-const { response } = require('express');
+const express = require('express');
 const request = require("request");
 var path = require('path');
 //Must be node-fetch v2
 var fetch = require('node-fetch');
 var appRouter = function (app) {
 
-  
+  app.use(express.static('html'))
+
+
   //Redirect to /home
   app.get('/',function(req,res){
     res.redirect('/home');
   });
   //Index page of site
   app.get('/home',function(req,res){
-    res.sendFile(path.join(__dirname+'/../public/index.html'));
+    res.sendFile(path.join(__dirname+'/../html/index.html'));
     //__dirname : It will resolve to your project folder.
   });
   //Testing url navigation
@@ -27,11 +29,11 @@ var appRouter = function (app) {
   app.get("/api", async function(req, res) {
     let api_url = "https://api.themoviedb.org/3/movie/550?api_key=6221e0ed54d6b02887581e40fa35381a";    //May want to store API key in hidden file for security
     let json = null;                                                                                    //Not sure why this variable is even used
-    const response = await fetch(api_url)                                                               //The glorious fetch()
-      .then((response) => json = response.json())                                                       //Parse result into json (probably not needed)
+    const data = await fetch(api_url)                                                               //The glorious fetch()
+      .then((data) => json = data.json())                                                       //Parse result into json (probably not needed)
       .catch((err) => console.log(err));                                                                //Output error if one occurs
     console.log(json);                                                                                  //Output json data to console (probably not needed)
-    res.json(response);                                                                                 //Response of this API call
+    res.json(data);                                                                                 //Response of this API call
   });
   }
   
